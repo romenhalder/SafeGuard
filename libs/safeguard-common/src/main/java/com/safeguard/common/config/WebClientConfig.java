29343f9604fcd32cd744aa48942cfa2941fcbf51
Romen <halderromen2002@gmail.com>
2026-08-06 15:57:41 +0530

    feat(infra): update Docker and shared library configuration

--- libs/safeguard-common/src/main/java/com/safeguard/common/config/WebClientConfig.java
+++ libs/safeguard-common/src/main/java/com/safeguard/common/config/WebClientConfig.java
@@ -1,16 +1,18 @@
 package com.safeguard.common.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class WebClientConfig {
 
+    /**
+     * RestClient builder for inter-service HTTP calls.
+     * Uses Spring MVC RestClient (Spring 6+) replacing the previous WebFlux WebClient.
+     */
     @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
